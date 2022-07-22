61c2b90b601428b8430a5a0c44141ea42a8c2210
JUNGEEYOU <junge2u@naver.com>
2022-07-22 22:48:09 +0900

    add 6-11 생성자

---
+++ javajungsuk_basic_src/_ch06/src/Ex6_11.java
@@ -0,0 +1,24 @@
+class Data_1 {
+    int val;
+}
+
+class Data_2 {
+    int val;
+//    public Data_2() {
+//
+//    }
+    Data_2(int x){
+        val = x;
+    }
+
+
+}
+
+
+public class Ex6_11 {
+    public static void main(String args[]) {
+        Data_1 d1 = new Data_1();
+        Data_2 d2 = new Data_2();   // 기본 생성자는 꼭 넣는 습관!!
+    }
+}
+
