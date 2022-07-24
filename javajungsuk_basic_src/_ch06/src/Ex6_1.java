5087110554856530710a84b9e5eebdd935ebf566
JUNGEEYOU <junge2u@naver.com>
2022-07-24 14:17:20 +0900

    6-1. 객체의 배열

--- javajungsuk_basic_src/_ch06/src/Ex6_1.java
+++ javajungsuk_basic_src/_ch06/src/Ex6_1.java
@@ -9,6 +9,18 @@ public class Ex6_1 {
         tv2 = tv;     // 참조변수를 저장 (서로 같은 곳을 참조)
         System.out.println(tv2.channel);   // tv2는 세팅 안했지만, tv와 동일한 값을 가짐
 
+        // 2. 객체의 배열
+        Tv[] tvarr = new Tv[3];
+
+        // tv 객체를 생성해서 참조된 주소를 직접 넣어줘야함
+        for(int i=0; i < tvarr.length; i++){
+            tvarr[i] = new Tv();
+            tvarr[i].channel = i + 10;
+        }
+
+        for(int i=0; i < tvarr.length; i++){
+            System.out.println(tvarr[i].channel);
+        }
     }
 }
 
--- javajungsuk_basic_src/_ch06/src/Ex6_11.java
+++ javajungsuk_basic_src/_ch06/src/Ex6_11.java
@@ -18,7 +18,7 @@ class Data_2 {
 public class Ex6_11 {
     public static void main(String args[]) {
         Data_1 d1 = new Data_1();
-        Data_2 d2 = new Data_2();   // 기본 생성자는 꼭 넣는 습관!!
+//        Data_2 d2 = new Data_2();   // 기본 생성자는 꼭 넣는 습관!!
     }
 }
 
